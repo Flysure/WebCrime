8b5c9d2ed7e77de90b193634511c574178a99805
Andrew <flysurea@gmail.com>
2018-10-27 14:30:35 -0400

    date range implementation

--- crimes/views.py
+++ crimes/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render, HttpResponse
 from crimes.models import CrimeTable
 from .forms import crimeForm
+import datetime
 
 def index(request):
-    crimes = CrimeTable.objects.filter(lat__icontains = '.')
+
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = crimeForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
+            beg = form.cleaned_data['date_reported_beg']
+            end = form.cleaned_data['date_reported_end']
+            beg = datetime.datetime.strptime(str(beg), '%Y-%m-%d').strftime('%m/%d/%y')
+            end = datetime.datetime.strptime(str(end), '%Y-%m-%d').strftime('%m/%d/%y')
+            crimes = CrimeTable.objects.filter( datereported__range = [beg, end], lat__icontains = '.' )
             return render(request,  'crimes/index.html', {'crimes':crimes, 'form':form})
 
     # if a GET (or any other method) we'll create a blank form
     else:
+        crimes = CrimeTable.objects.filter(lat__icontains = '.')
         form = crimeForm()
 
     return render(request,  'crimes/index.html', {'crimes':crimes, 'form':form})
