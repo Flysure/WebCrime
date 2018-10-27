29eee379ceab0a8946f30709dd317591a60c5e5a
Andrew <flysurea@gmail.com>
2018-10-27 12:48:09 -0400

    navbar and sidebar

--- crimes/views.py
+++ crimes/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, HttpResponse
 from crimes.models import CrimeTable
 
 def index(request):
-    crimes = CrimeTable.objects.filter(lat__icontains= '.')
+    crimes = CrimeTable.objects.filter(lat__icontains = '.')
     return render(request,  'crimes/index.html', {'crimes':crimes})
